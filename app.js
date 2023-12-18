c6aee670f895b54a33eb8ec9d43a8dfe80e93a8d
Muhammad-Bilal-Vayani <143824973+Muhammad-Bilal-Vayani@users.noreply.github.com>
2023-12-18 16:52:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,113 @@
+let arr = [
+    {
+        brand: "Samsung",
+        model: "SamsungA30",
+        price: "40000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Samsung",
+        model: "SamsungA10",
+        price: "40000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Samsung",
+        model: "SamsungA20",
+        price: "40000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Vivo",
+        model: "VivoY20",
+        price: "20000",
+        camera: "40px",
+        ram: "2gb",
+        rom : "32gb",
+        
+    },
+    {
+        brand: "Vivo",
+        model: "VivoY11",
+        price: "20000",
+        camera: "40px",
+        ram: "3gb",
+        rom : "32gb",
+        
+    },
+    {
+        brand: "Vivo",
+        model: "VivoY15",
+        price: "30000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "64gb",
+        
+    },
+    {
+        brand: "Motorola",
+        model: "Motorola123",
+        price: "5000",
+        camera: "2px",
+        ram: "2gb",
+        rom : "32gb",
+        
+    },
+    {
+        brand: "Iphone",
+        model: "Iphone12",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Iphone",
+        model: "Iphone13",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Iphone",
+        model: "IphoneX",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+]
+    
+
+    function searchMobilePhones() {
+      const searchType = document.getElementById('searchType').value;
+      const searchInput = document.getElementById('searchInput').value.toLowerCase();
+
+      const resultList = document.getElementById('resultList');
+      resultList.innerHTML = ''; // Clear previous results
+
+      const filteredPhones = arr.filter(phone => {
+        const phoneValue = phone[searchType].toLowerCase();
+        return phoneValue.includes(searchInput);
+      });
+
+      // Display the filtered results
+      filteredPhones.forEach(phone => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Brand: ${phone.brand}, Model: ${phone.model}, Price: ${phone.price}, Camera: ${phone.camera}, RAM: ${phone.ram}, ROM: ${phone.rom}`;
+        resultList.appendChild(listItem);
+      });
+    }
